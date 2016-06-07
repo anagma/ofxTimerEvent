05343de9ef63b9d67b3ddb1ba30353d1e7ae1964
FURUDATE Ken <anagma@FURUDATEMacBook-Pro.local>
2016-06-07 12:14:38 +0900

    oops...

--- src/ofxTimerEvent.h
+++ src/ofxTimerEvent.h
@@ -11,7 +11,6 @@
 
 #include "ofMain.h"
 
-<<<<<<< HEAD
 #define OFX_TIMER_EVENT_TIME_RESOLUTION_DEFAULT 1
 
 template<class EventArg = string>
@@ -60,62 +59,6 @@ public:
 		if(! isThreadRunning()){
 			startThread();
 		}
-=======
-#define OFX_TIMER_EVENT_TIME_RESOLUTION_DEFAULT 5
-
-class ofxTimerEvent : ofThread{
-	
-	long long startTimeMillis;
-	map<string, long long> keyframes;
-	long long duration;
-	
-	long long lastUpdatedTime;
-	int timeResolution;
-	int repeatCount;
-	
-	bool bLoop;
-	bool bDurationSetManually;
-	
-public:
-	
-	ofEvent<string> keyframe;
-	ofEvent<void> loop;
-	ofEvent<void> finish;
-	
-	ofxTimerEvent(){
-		// default
-		timeResolution = OFX_TIMER_EVENT_TIME_RESOLUTION_DEFAULT;
-		duration = -1;
-		bLoop = false;
-		startTimeMillis = -1;
-		lastUpdatedTime = -1;
-		repeatCount = 0;
-		bDurationSetManually = false;
-	}
-	
-	void start(long long startOffset=0){
-		if(keyframes.size() <= 0){
-			ofLogError("ofxTimerEvent") << "keyframes is empty. stop";
-		}
-		
-		
-		if(! bDurationSetManually){
-			duration = getFinalKeyframeTime();
-		}
-		
-		long long elapsedTimeMillis = ofGetElapsedTimeMillis();
-		
-		startTimeMillis	= elapsedTimeMillis-startOffset;
-		lastUpdatedTime	= elapsedTimeMillis-startTimeMillis;
-		repeatCount		= lastUpdatedTime/duration;
-
-		if(! isThreadRunning()){
-//			stopThread();
-			startThread(true);
-		}
-//		ofSleepMillis(1000);
-		getPocoThread().setName("ofxTimerEventThread");
->>>>>>> origin/master
 	}
 	void seek(long long time){
 		start(time);
@@ -124,7 +67,6 @@ public:
 		stopThread();
 	}
 	long long getNow(){
-<<<<<<< HEAD
 		if(isRunning()){
 			return (ofGetElapsedTimeMillis()-startTimeMillis)%getDuration();
 		}
@@ -203,23 +145,6 @@ public:
 	EventArg * getLastKeyframe(){
 		if(! isRunning()){
 			return NULL;
-=======
-		return ofGetElapsedTimeMillis()-startTimeMillis;
-	}
-	void addKeyFrame(string name, long long time){
-		keyframes.insert(make_pair(name, time));
-		if(! bDurationSetManually){
-			duration = getFinalKeyframeTime();
-		}
-	}
-	void removeKeyFrame(string name){
-		map<string, long long>::iterator it = keyframes.find(name);
-		if(it != keyframes.end()){
-			keyframes.erase(it);
-			if(! bDurationSetManually){
-				duration = getFinalKeyframeTime();
-			}
->>>>>>> origin/master
 		}
 		long long now = getNow();
 		
@@ -235,7 +160,6 @@ public:
 		return nearest->second;
 	}
 	
-<<<<<<< HEAD
 	EventArg * getNextKeyframe(){
 		if(! isRunning()){
 			return NULL;
@@ -255,82 +179,7 @@ public:
 	
 	ofxTimerEventKeyframes<EventArg> & getKeyframes(){
 		return keyframes;
-=======
-	bool isRunning(){
-		return isThreadRunning();
 	}
-	
-	void clear(){
-		stopThread();
-		keyframes.clear();
-		if(! bDurationSetManually){
-			duration = -1;
-		}
-		
-	}
-	
-	// resolution
-	void setTimeResolution(int _res){
-		timeResolution = _res;
-	}
-	int getTimeResolution(){
-		return timeResolution;
-	}
-	
-	// duration
-	void setDuration(long long _duration){
-		duration = _duration;
-		if(_duration >= 0){
-			bDurationSetManually = true;
-		}else{
-			bDurationSetManually = false;
-		}
-	}
-	long long getDuration(){
-		if(bDurationSetManually){
-			return duration;
-		}
-		
-		// get last keyframe
-		duration = getFinalKeyframeTime();
-		return duration;
-	}
-	
-	// loop
-	void setLoop(bool _loop){
-		bLoop = _loop;
-	}
-	bool isLoop(){
-		return bLoop;
->>>>>>> origin/master
-	}
-	
-	string getLastKeyframe(){
-		if(! isRunning()){
-			return "";
-		}
-		long long now = getNow();
-		if(!bLoop && now > duration){
-			return "";
-		}
-		now %= duration;
-		pair<string, long long> nearest = make_pair("", -1);
-		bool bFirst = true;
-		for(auto & k : keyframes){
-			if (k.second < now) {
-				if(bFirst){
-					bFirst = false;
-					nearest = k;
-				}else{
-					if(now-k.second < now-nearest.second){
-						nearest = k;
-					}
-				}
-			}
-		}
-		return nearest.first;
-	}
-	
 	
 protected:
 	
@@ -350,7 +199,6 @@ protected:
 		while(isThreadRunning()){
 			long long now = getNow();
 			if(now > 0){
-<<<<<<< HEAD
 				if(bLoop && now < lastUpdatedTime){
 					// loop!!
 					ofLogNotice("ofxTimerEventThread") << "Timer looped";
@@ -359,30 +207,10 @@ protected:
 					// trigger rest events
 					for(auto it = keyframes.upper_bound(lastUpdatedTime); it != keyframes.end(); it++){
 						triggetEvent(it->second);
-=======
-				if(bLoop){
-					int newRepeatCount = now/duration;
-					if(repeatCount != newRepeatCount){
-						repeatCount = newRepeatCount;
-						if(repeatCount > 0){
-							
-							// trigger all rest event
-							for(auto & k : keyframes){
-								if(k.second >= lastUpdatedTime && k.second <= duration){
-									string arg = k.first;
-									ofNotifyEvent(keyframe, arg, this);
-								}
-							}
-							ofLogNotice("ofxTimerEventThread") << "Timer looped: " << repeatCount << ", " << now;
-							ofNotifyEvent(loop, this);
-						}
-						lastUpdatedTime = 0;
->>>>>>> origin/master
 					}
 					lastUpdatedTime = -1;
 				}
 				
-<<<<<<< HEAD
 				auto itLow = keyframes.upper_bound(lastUpdatedTime);
 				auto itUp = keyframes.upper_bound(now);
 				
@@ -393,24 +221,11 @@ protected:
 				}
 				
 				
-=======
-				for(auto & k : keyframes){
-					if(k.second >= lastUpdatedTime && k.second < now){
-						string arg = k.first;
-						ofNotifyEvent(keyframe, arg, this);
-					}
-				}
-
->>>>>>> origin/master
 				lastUpdatedTime = now;
 				
 			}
 			
-<<<<<<< HEAD
 			if(! bLoop && now > getDuration()){
-=======
-			if(! bLoop && now > duration){
->>>>>>> origin/master
 				ofLogNotice("ofxTimerEventThread") << "Reached to final keyframe. stop thread";
 				ofNotifyEvent(finish, this);
 				break;
@@ -419,7 +234,6 @@ protected:
 		}
 	}
 	
-<<<<<<< HEAD
 	ofxTimerEventKeyframesEntry<EventArg> * getFinalKeyframe(){
 		if(keyframes.empty()){
 			return NULL;
@@ -430,16 +244,6 @@ protected:
 	
 	void triggetEvent(EventArg & arg){
 		ofNotifyEvent(keyframe, arg, this);
-=======
-	long long getFinalKeyframeTime(){
-		long long time = 0;
-		for(auto & k : keyframes){
-			if(k.second >= duration){
-				time = k.second;
-			}
-		}
-		return time;
->>>>>>> origin/master
 	}
 };
 
